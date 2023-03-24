e41a1c6210fc4797d664bc91536199d58fae9ce4
Sharuda <sharuda.yana98@gmail.com>
2023-03-24 21:26:03 +0200

    add fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,15 @@
-const BASE_URL = `https://restcountries.eu`;
+const BASE_URL = `https://restcountries.com/v3.1/name/`;
 
-export const fetchCountries = name =>
-  fetch(`${BASE_URL}/rest/v2/name/${name}`).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
+export const fetchCountries = name => {
+  return fetch(
+    `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
 
-    return res.json();
-  });
+      return res.json();
+    })
+    .catch(err => console.log(err));
+};
--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
 
 const handleRequestNameCountry = event => {
   const searchQuery = event.target.value.trim();
 
-  fetchCountries(searchQuery).then(data => console.log(data));
+  fetchCountries(searchQuery).then(data => {
+    if (data.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+      return;
+    } else if (data.length <= 10 && data.length >= 2) {
+      
+    }
+  });
 };
 
 inputEl.addEventListener(
